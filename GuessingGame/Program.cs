31e4fb9c0e36d4f8e34698a548050d45d1d4d672
hcrudge <hcrudge1@gmail.com>
2021-08-26 09:07:10 -0500

    initial commit and Phase 1-2

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 42;
+            Console.Write("Guess the secret number: ");
+            string answer = Console.ReadLine().ToLower();
+            // Console.Write(answer);
+            if(secretNumber == int.Parse(answer))
+            {
+                Console.WriteLine("You guessed it!!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect! Try again.");
+            }
+        }
+    }
+}
