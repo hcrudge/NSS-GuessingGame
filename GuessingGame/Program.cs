d6accf181673081662e5da8c4ccabae5c111b6dc
hcrudge <hcrudge1@gmail.com>
2021-08-26 09:38:20 -0500

    Phase 4 complete

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,22 +10,25 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             List<int> Guesses = new List<int>();
-            while( Guesses.Count < 4)
+            while (Guesses.Count < 4)
             {
-            int secretNumber = 42;
-            Console.Write("Guess the secret number: ");
-            string answer = Console.ReadLine().ToLower();
-            int numAnswer = int.Parse(answer);
-            // Console.Write(numAnswer);
-            if(secretNumber == numAnswer)
-            {
-                Console.WriteLine("You guessed it!!");
-            }
-            else
-            {
-                Console.WriteLine("Incorrect! Try again.");
-                Guesses.Add(numAnswer);
-            }
+                int secretNumber = 42;
+                Console.WriteLine("Guess the secret number. ");
+                Console.Write($"Your guess({Guesses.Count + 1})>");
+                string answer = Console.ReadLine().ToLower();
+                int numAnswer = int.Parse(answer);
+                // Console.Write(numAnswer);
+                if (secretNumber == numAnswer)
+                {
+                    Console.WriteLine("You guessed it!!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect! Try again.");
+                    Guesses.Add(numAnswer);
+
+                }
             };
         }
     }
